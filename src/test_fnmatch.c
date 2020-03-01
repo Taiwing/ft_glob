121dfb7ccb534a9353f750a08e58519c7ae1c1b7
Taiwing <yoyo.fofo47@gmail.com>
2020-03-01 04:46:26 +0100

    Reorganize tests

--- src/test_fnmatch.c
+++ src/test_fnmatch.c
@@ -3,482 +3,445 @@
 #include "c_colors.h"
 #include "ft_fnmatch.h"
 
-char	*test1[] =
-{
-	"[:alpha:]o[\\:][!\\!]ol",
-	"lo:!ol",
-	"lo:xol",
-	NULL
-};
+#define	MAX_NAME_STRINGS	32
 
-char	*test2[] =
+char	*tests[][MAX_NAME_STRINGS] =
 {
-	"[:alpha:]*o*[\\:]*[adfxsfsf]ol*",
-	"lo:!ol",
-	"lo:xol",
-	"aRANDOMoRANDOM:RANDaolRAND",
-	"ARANDoRAND::::aol",
-	"ARANDoRAND::::ao",
-	"lRANDoRAND::::aol",
-	"pRANDoRAND::::ao",
-	NULL
-};
-
-char	*test3[] =
-{
-	"[\\][]]]]\\[\\[\\[wut\\?[\\?a-z:digit:]\\????.lol",
-	"]]]][[[wut???wut.lol",
-	"[]]][[[wut???wut.lol",
-	"[]]][[[wut?a?wut.lol",
-	"[]]][[[wut?k?wut.lol",
-	"[]]][[[wut?z?wut.lol",
-	"[]]][[[wut?1?wut.lol",
-	"[]]][[[wut?7?wut.lol",
-	"[]]][[[wut?9?wut.lol",
-	NULL
-};
-
-char	*test4[] =
-{
-	"[\\][]]]]\\[\\[\\[wut\\?[:alnum:]\\????.lol",
-	"]]]][[[wut???wut.lol",
-	"[]]][[[wut???wut.lol",
-	"[]]][[[wut?a?wut.lol",
-	"[]]][[[wut?k?wut.lol",
-	"[]]][[[wut?z?wut.lol",
-	"[]]][[[wut?1?wut.lol",
-	"[]]][[[wut?7?wut.lol",
-	"[]]][[[wut?9?wut.lol",
-	NULL
-};
-
-char	*test5[] =
-{
-	"[a-z345A-Z][a-zCDE0-9][0-9abcA-Z]",
-	"ab9",
-	"aE9",
-	"aEa",
-	"45b",
-	"BCE",
-	"2ab",
-	"aF0",
-	"aaa",
-	"aaaa",
-	"bbb",
-	"bbba",
-	NULL
-};
-
-char	*test6[] =
-{
-	"_---[\\-][!a-z][\\-]---_",
-	"_----A----_",
-	"_----B----_",
-	"_----a----_",
-	"_----b----_",
-	NULL
-};
-
-char	*test7[] =
-{
-	"_---[\\-][-----][\\-]---_",
-	"_---------_",
-	"_----B----_",
-	"_----a----_",
-	"_----b----_",
-	NULL
-};
-
-char	*test8[] =
-{
-	"_---[\\-][-][\\-]---_",
-	"_---------_",
-	"_----B----_",
-	"_----a----_",
-	"_----b----_",
-	NULL
-};
-
-char	*test9[] =
-{
-	"_---[\\-][--][\\-]---_",
-	"_---------_",
-	"_----B----_",
-	"_----a----_",
-	"_----b----_",
-	NULL
-};
-
-char	*test10[] =
-{
-	"_---[\\-][---][\\-]---_",
-	"_---------_",
-	"_----B----_",
-	"_----a----_",
-	"_----b----_",
-	NULL
-};
-
-char	*test11[] =
-{
-	"[a-z-~]",
-	"a",
-	"b",
-	"~",
-	"-",
-	"{",
-	"|",
-	"}",
-	"A",
-	"B",
-	"C",
-	NULL
-};
-
-char	*test12[] =
-{
-	"[a-z\\-z-~]",
-	"a",
-	"b",
-	"~",
-	"-",
-	"{",
-	"|",
-	"}",
-	"A",
-	"B",
-	"C",
-	NULL
-};
-
-char	*test13[] =
-{
-	"[z-a\\-~-z]",
-	"a",
-	"b",
-	"~",
-	"-",
-	"{",
-	"|",
-	"}",
-	"A",
-	"B",
-	"C",
-	NULL
-};
-
-char	*test14[] =
-{
-	"abc\\\\[abc]\\\\[def]\\\\[ghi]\\\\\\*\\*\\*\\",
-	"abc\\a\\d\\g\\***\\\\",
-	"abc\\a\\d\\g\\***\\",
-	"abc\\b\\e\\h\\***",
-	"abc\\c\\f\\i\\***",
-	NULL
-};
-
-char	*test15[] =
-{
-	"abc\\\\[abc]\\\\[def]\\\\[ghi]\\\\\\*\\*\\*\\\\",
-	"abc\\a\\d\\g\\***\\\\",
-	"abc\\a\\d\\g\\***\\",
-	"abc\\b\\e\\h\\***",
-	"abc\\c\\f\\i\\***",
-	NULL
-};
+	{
+		"[:alpha:]o[\\:][!\\!]ol",
+		"lo:!ol",
+		"lo:xol",
+		NULL
+	},
+	{
+		"[:alpha:]*o*[\\:]*[adfxsfsf]ol*",
+		"lo:!ol",
+		"lo:xol",
+		"aRANDOMoRANDOM:RANDaolRAND",
+		"ARANDoRAND::::aol",
+		"ARANDoRAND::::ao",
+		"lRANDoRAND::::aol",
+		"pRANDoRAND::::ao",
+		NULL
+	},
 
 
-char	*test16[] =
-{
-	"abc\\\\[abc]\\\\[def]\\\\[ghi]\\\\\\*\\*\\*",
-	"abc\\a\\d\\g\\***",
-	"abc\\b\\e\\h\\***",
-	"abc\\c\\f\\i\\***",
-	NULL
-};
+	{
+		"[\\][]]]]\\[\\[\\[wut\\?[\\?a-z:digit:]\\????.lol",
+		"]]]][[[wut???wut.lol",
+		"[]]][[[wut???wut.lol",
+		"[]]][[[wut?a?wut.lol",
+		"[]]][[[wut?k?wut.lol",
+		"[]]][[[wut?z?wut.lol",
+		"[]]][[[wut?1?wut.lol",
+		"[]]][[[wut?7?wut.lol",
+		"[]]][[[wut?9?wut.lol",
+		NULL
+	},
 
-char	*test17[] =
-{
-	"[[[[[[[[[[[",
-	"[[[[[[[[[[[",
-	NULL
-};
 
-char	*test18[] =
-{
-	"[[[[[[[[[[[????",
-	"[[[[[[[[[[[asdf",
-	NULL
-};
+	{
+		"[\\][]]]]\\[\\[\\[wut\\?[:alnum:]\\????.lol",
+		"]]]][[[wut???wut.lol",
+		"[]]][[[wut???wut.lol",
+		"[]]][[[wut?a?wut.lol",
+		"[]]][[[wut?k?wut.lol",
+		"[]]][[[wut?z?wut.lol",
+		"[]]][[[wut?1?wut.lol",
+		"[]]][[[wut?7?wut.lol",
+		"[]]][[[wut?9?wut.lol",
+		NULL
+	},
 
-char	*test19[] =
-{
-	"[[[[[[[[[[[]????*lol",
-	"[[[[[[[[[[[]asdf123456789lol",
-	NULL
-};
 
-char	*test20[] =
-{
-	"/.root/.*/*/[a-z0-9]ol",
-	"/.root/.invisible_subdir/other_subdir/lol",
-	"/.root/.invisible_subdir/other_subdir/9ol",
-	"*.root/.invisible_subdir/other_subdir/9ol",
-	"*/.root/.invisible_subdir/other_subdir/9ol",
-	"/.root/.invisible_subdir/extra_subdir/other_subdir/9ol",
-	NULL
-};
+	{
+		"[a-z345A-Z][a-zCDE0-9][0-9abcA-Z]",
+		"ab9",
+		"aE9",
+		"aEa",
+		"45b",
+		"BCE",
+		"2ab",
+		"aF0",
+		"aaa",
+		"aaaa",
+		"bbb",
+		"bbba",
+		NULL
+	},
 
-char	*test21[] =
-{
-	"/.root/?invisible_subdir/other_subdir/9ol",
-	"/.root/.invisible_subdir/other_subdir/9ol",
-	NULL
-};
 
-char	*test22[] =
-{
-	"/?root/.invisible_subdir/other_subdir/9ol",
-	"/.root/.invisible_subdir/other_subdir/9ol",
-	NULL
-};
+	{
+		"_---[\\-][!a-z][\\-]---_",
+		"_----A----_",
+		"_----B----_",
+		"_----a----_",
+		"_----b----_",
+		NULL
+	},
 
-char	*test23[] =
-{
-	"/.root/.invisible_subdir/other_subdir?9ol",
-	"/.root/.invisible_subdir?other_subdir/9ol",
-	NULL
-};
 
-char	*test24[] =
-{
-	"/.*/.invisible_subdir/*/9ol",
-	"/.root/.invisible_subdir/other_subdir/9ol",
-	NULL
-};
+	{
+		"_---[\\-][-----][\\-]---_",
+		"_---------_",
+		"_----B----_",
+		"_----a----_",
+		"_----b----_",
+		NULL
+	},
 
-char	*test25[] =
-{
-	"/.root/.invisible_subdir/other_subdir*9ol",
-	"/.root/.invisible_subdir/other_subdir/9ol",
-	NULL
-};
 
-char	*test26[] =
-{
-	"/.root/.invisible_subdir/other_subdi*/9ol",
-	"/.root/.invisible_subdir/other_subdir/9ol",
-	NULL
-};
+	{
+		"_---[\\-][-][\\-]---_",
+		"_---------_",
+		"_----B----_",
+		"_----a----_",
+		"_----b----_",
+		NULL
+	},
 
-char	*test27[] =
-{
-	"*root/.invisible_subdir/other_subdir/9ol",
-	"/.root/.invisible_subdir/other_subdir/9ol",
-	NULL
-};
 
-char	*test28[] =
-{
-	"/.root??invisible_subdir/other_subdir/9ol",
-	"/.root/.invisible_subdir/other_subdir/9ol",
-	NULL
-};
+	{
+		"_---[\\-][--][\\-]---_",
+		"_---------_",
+		"_----B----_",
+		"_----a----_",
+		"_----b----_",
+		NULL
+	},
 
-char	*test29[] =
-{
-	"??root??invisible_subdir?other_subdir?9ol",
-	"/.root/.invisible_subdir/other_subdir/9ol",
-	NULL
-};
 
-char	*test30[] =
-{
-	"/.*/.*/*/*",
-	"/.root/.invisible_subdir/other_subdir/9ol",
-	"/.root/.invisible_subdir/other_subdir/9ol.",
-	"/.root/.invisible_subdir/other_subdir/9ol/",
-	"/.root/.invisible_subdir/other_subdir/9ol//",
-	"/.root/.invisible_subdir/other_subdir//9ol",
-	NULL
-};
+	{
+		"_---[\\-][---][\\-]---_",
+		"_---------_",
+		"_----B----_",
+		"_----a----_",
+		"_----b----_",
+		NULL
+	},
 
-char	*test31[] =
-{
-	".*/.*/*/*",
-	".root/.invisible_subdir/other_subdir/9ol",
-	".root/.invisible_subdir/other_subdir/9ol.",
-	".root/.invisible_subdir/other_subdir/9ol/",
-	".root/.invisible_subdir/other_subdir/9ol//",
-	".root/.invisible_subdir/other_subdir//9ol",
-	NULL
-};
 
-char	*test32[] =
-{
-	"*/.*/*/*",
-	"root/.invisible_subdir/other_subdir/9ol",
-	"root/.invisible_subdir/other_subdir/9ol.",
-	"root/.invisible_subdir/other_subdir/9ol/",
-	"root/.invisible_subdir/other_subdir/9ol//",
-	"root/.invisible_subdir/other_subdir//9ol",
-	NULL
-};
+	{
+		"[a-z-~]",
+		"a",
+		"b",
+		"~",
+		"-",
+		"	{",
+		"|",
+		"}",
+		"A",
+		"B",
+		"C",
+		NULL
+	},
 
-char	*test33[] =
-{
-	"lol/myass/wut",
-	"lol/myass/wut",
-	NULL
-};
 
-char	*test34[] =
-{
-	"*/myass/wut",
-	"lol/myass/wut",
-	NULL
-};
+	{
+		"[a-y\\-z-~]",
+		"a",
+		"b",
+		"z",
+		"~",
+		"-",
+		"	{",
+		"|",
+		"}",
+		"A",
+		"B",
+		"C",
+		NULL
+	},
 
-char	*test35[] =
-{
-	"lol/*/wut",
-	"lol/myass/wut",
-	NULL
-};
 
-char	*test36[] =
-{
-	"lol/myass/*",
-	"lol/myass/wut",
-	NULL
-};
+	{
+		"[a-yz-~]",
+		"a",
+		"b",
+		"z",
+		"~",
+		"-",
+		"	{",
+		"|",
+		"}",
+		"A",
+		"B",
+		"C",
+		NULL
+	},
 
-char	*test37[] =
-{
-	"*/myass/*",
-	"lol/myass/wut",
-	NULL
-};
 
-char	*test38[] =
-{
-	"*/*/wut",
-	"lol/myass/wut",
-	NULL
-};
+	{
+		"[!]",
+		"!",
+		"a",
+		"C",
+		"",
+		NULL
+	},
 
-char	*test39[] =
-{
-	"*/*/*",
-	"lol/myass/wut",
-	NULL
-};
 
-char	*test40[] =
-{
-	"lol/wut",
-	"lol/wut",
-	NULL
-};
 
-char	*test41[] =
-{
-	"*/wut",
-	"lol/wut",
-	NULL
-};
+	{
+		"[z-a\\-~-z]",
+		"a",
+		"b",
+		"~",
+		"-",
+		"	{",
+		"|",
+		"}",
+		"A",
+		"B",
+		"C",
+		NULL
+	},
 
-char	*test42[] =
-{
-	"lol/*",
-	"lol/wut",
-	NULL
-};
 
-char	*test43[] =
-{
-	"lol/*/",
-	"lol/wut/",
-	"lol/w/",
-	"lol//",
-	"lol/wut",
-	NULL
-};
+	{
+		"abc\\\\[abc]\\\\[def]\\\\[ghi]\\\\\\*\\*\\*\\",
+		"abc\\a\\d\\g\\***\\\\",
+		"abc\\a\\d\\g\\***\\",
+		"abc\\b\\e\\h\\***",
+		"abc\\c\\f\\i\\***",
+		NULL
+	},
 
-char	*test44[] =
-{
-	"*/*",
-	"lol/wut",
-	"lol/wut/",
-	"lol//",
-	NULL
-};
 
-char	*test45[] =
-{
-	"*lol/wut*",
-	"lol/wut",
-	"lol/wut/",
-	NULL
-};
+	{
+		"abc\\\\[abc]\\\\[def]\\\\[ghi]\\\\\\*\\*\\*\\\\",
+		"abc\\a\\d\\g\\***\\\\",
+		"abc\\a\\d\\g\\***\\",
+		"abc\\b\\e\\h\\***",
+		"abc\\c\\f\\i\\***",
+		NULL
+	},
 
-char	*test46[] =
-{
-	"*/",
-	"l/",
-	"lol/",
-	NULL
-};
 
-char	**tests[] =
-{
-	test1,
-	test2,
-	test3,
-	test4,
-	test5,
-	test6,
-	test7,
-	test8,
-	test9,
-	test10,
-	test11,
-	test12,
-	test13,
-	test14,
-	test15,
-	test16,
-	test17,
-	test18,
-	test19,
-	test20,
-	test21,
-	test22,
-	test23,
-	test24,
-	test25,
-	test26,
-	test27,
-	test28,
-	test29,
-	test30,
-	test31,
-	test32,
-	test33,
-	test34,
-	test35,
-	test36,
-	test37,
-	test38,
-	test39,
-	test40,
-	test41,
-	test42,
-	test43,
-	test44,
-	test45,
-	test46,
-	NULL
+
+	{
+		"abc\\\\[abc]\\\\[def]\\\\[ghi]\\\\\\*\\*\\*",
+		"abc\\a\\d\\g\\***",
+		"abc\\b\\e\\h\\***",
+		"abc\\c\\f\\i\\***",
+		NULL
+	},
+
+
+	{
+		"[[[[[[[[[[[",
+		"[[[[[[[[[[[",
+		NULL
+	},
+
+
+	{
+		"[[[[[[[[[[[????",
+		"[[[[[[[[[[[asdf",
+		NULL
+	},
+
+
+	{
+		"[[[[[[[[[[[]????*lol",
+		"[[[[[[[[[[[]asdf123456789lol",
+		NULL
+	},
+
+
+	{
+		"/.root/.*/*/[a-z0-9]ol",
+		"/.root/.invisible_subdir/other_subdir/lol",
+		"/.root/.invisible_subdir/other_subdir/9ol",
+		"*.root/.invisible_subdir/other_subdir/9ol",
+		"*/.root/.invisible_subdir/other_subdir/9ol",
+		"/.root/.invisible_subdir/extra_subdir/other_subdir/9ol",
+		NULL
+	},
+
+
+	{
+		"/.root/?invisible_subdir/other_subdir/9ol",
+		"/.root/.invisible_subdir/other_subdir/9ol",
+		NULL
+	},
+
+
+	{
+		"/?root/.invisible_subdir/other_subdir/9ol",
+		"/.root/.invisible_subdir/other_subdir/9ol",
+		NULL
+	},
+
+
+	{
+		"/.root/.invisible_subdir/other_subdir?9ol",
+		"/.root/.invisible_subdir?other_subdir/9ol",
+		NULL
+	},
+
+
+	{
+		"/.*/.invisible_subdir/*/9ol",
+		"/.root/.invisible_subdir/other_subdir/9ol",
+		NULL
+	},
+
+
+	{
+		"/.root/.invisible_subdir/other_subdir*9ol",
+		"/.root/.invisible_subdir/other_subdir/9ol",
+		NULL
+	},
+
+
+	{
+		"/.root/.invisible_subdir/other_subdi*/9ol",
+		"/.root/.invisible_subdir/other_subdir/9ol",
+		NULL
+	},
+
+
+	{
+		"*root/.invisible_subdir/other_subdir/9ol",
+		"/.root/.invisible_subdir/other_subdir/9ol",
+		NULL
+	},
+
+
+	{
+		"/.root??invisible_subdir/other_subdir/9ol",
+		"/.root/.invisible_subdir/other_subdir/9ol",
+		NULL
+	},
+
+	{
+		"??root??invisible_subdir?other_subdir?9ol",
+		"/.root/.invisible_subdir/other_subdir/9ol",
+		NULL
+	},
+
+	{
+		"/.*/.*/*/*",
+		"/.root/.invisible_subdir/other_subdir/9ol",
+		"/.root/.invisible_subdir/other_subdir/9ol.",
+		"/.root/.invisible_subdir/other_subdir/9ol/",
+		"/.root/.invisible_subdir/other_subdir/9ol//",
+		"/.root/.invisible_subdir/other_subdir//9ol",
+		NULL
+	},
+
+	{
+		".*/.*/*/*",
+		".root/.invisible_subdir/other_subdir/9ol",
+		".root/.invisible_subdir/other_subdir/9ol.",
+		".root/.invisible_subdir/other_subdir/9ol/",
+		".root/.invisible_subdir/other_subdir/9ol//",
+		".root/.invisible_subdir/other_subdir//9ol",
+		NULL
+	},
+
+	{
+		"*/.*/*/*",
+		"root/.invisible_subdir/other_subdir/9ol",
+		"root/.invisible_subdir/other_subdir/9ol.",
+		"root/.invisible_subdir/other_subdir/9ol/",
+		"root/.invisible_subdir/other_subdir/9ol//",
+		"root/.invisible_subdir/other_subdir//9ol",
+		NULL
+	},
+
+	{
+		"lol/myass/wut",
+		"lol/myass/wut",
+		NULL
+	},
+
+	{
+		"*/myass/wut",
+		"lol/myass/wut",
+		NULL
+	},
+
+	{
+		"lol/*/wut",
+		"lol/myass/wut",
+		NULL
+	},
+
+	{
+		"lol/myass/*",
+		"lol/myass/wut",
+		NULL
+	},
+
+	{
+		"*/myass/*",
+		"lol/myass/wut",
+		NULL
+	},
+
+	{
+		"*/*/wut",
+		"lol/myass/wut",
+		NULL
+	},
+
+	{
+		"*/*/*",
+		"lol/myass/wut",
+		NULL
+	},
+
+	{
+		"lol/wut",
+		"lol/wut",
+		NULL
+	},
+
+	{
+		"*/wut",
+		"lol/wut",
+		NULL
+	},
+
+	{
+		"lol/*",
+		"lol/wut",
+		NULL
+	},
+
+	{
+		"lol/*/",
+		"lol/wut/",
+		"lol/w/",
+		"lol//",
+		"lol/wut",
+		NULL
+	},
+
+	{
+		"*/*",
+		"lol/wut",
+		"lol/wut/",
+		"lol//",
+		NULL
+	},
+
+	{
+		"*lol/wut*",
+		"lol/wut",
+		"lol/wut/",
+		NULL
+	},
+
+	{
+		"*/",
+		"l/",
+		"lol/",
+		NULL
+	},
+	{NULL}
 };
 
 static int	exec_fnmatch_test(const char *pattern, const char *name)
@@ -507,14 +470,14 @@ int	 	test_fnmatch(void)
 	char	**name_string;
 
 	ret = 0;
-	while (tests[++i])
+	while (tests[++i][0])
 	{
 		pattern = tests[i][0];
 		printf("pattern '%s':\n", pattern);
 		name_string = tests[i];
 		while (*++name_string)
 			ret += exec_fnmatch_test(pattern, *name_string);
-		if (tests[i + 1])
+		if (tests[i + 1][0])
 			puts("");
 	}
 	return (ret);
